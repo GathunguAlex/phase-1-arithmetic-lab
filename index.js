531cc5403d4e6fd0a5d5438ac594d1a804ee4ce7
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-09-02 01:15:44 +0300

    takeTwo

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+
+
+let rdm = (math.floor(math.random() * 10) + 1);
+
+//remainder equal to 4
+
+let num3 = 34;
+let num4 = 6;
+let remainder;
+remainder = (num3 % num4);
+
+let a = 11;
+let b = 13;
+let c = 2;
+let d = 20;
+let max;
+max = (math.max(a, b, c, d));
